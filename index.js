30aa492e69cb2120120f443f5ab2dc3075286b3b
github username <evan@heyevan.com>
2022-02-18 08:40:37 -0800

    adds click me cue

--- index.js
+++ index.js
@@ -28,7 +28,7 @@ const poetrySquare = () => {
     poetry.addEventListener('mouseenter', function(){
     poetry.style.backgroundColor="#fe6d0b";
     const a = document.createElement('a')
-    const p = document.createTextNode('A slightly embarrasing, kinda cool video of me reciting poetry 9 years ago.');
+    const p = document.createTextNode('A kinda embarrasing/cool video of me reciting poetry 9 years ago');
     a.appendChild(p)
     a.href="https://www.youtube.com/watch?v=e-ZC-BY63u4"
     poetry.appendChild(a);
@@ -41,6 +41,42 @@ const poetrySquare = () => {
 }
 
 poetrySquare();
+
+const coopSquare = () => {
+    const coop = document.getElementById("co-op")
+    coop.addEventListener('mouseenter', function(){
+    coop.style.backgroundColor="#7499c3";
+    const a = document.createElement('a')
+    const p = document.createTextNode('A website for a healthcare cooperative I helped put together');
+    a.appendChild(p)
+    a.href="https://www.obranhealth.com/"
+    coop.appendChild(a)
+    })
+    coop.addEventListener('mouseleave', function(){
+    coop.style.backgroundColor="yellow";
+    coop.innerHTML=""
+    })
+}
+
+coopSquare();
+
+const microphoneSquare = () => {
+    const phone = document.getElementById("microphone")
+    phone.addEventListener('mouseenter', function(){
+    phone.style.backgroundColor="c2c7ca";
+    const a = document.createElement('a')
+    const p = document.createTextNode('A vintage phone I hacked to be a microphone');
+    a.appendChild(p)
+    a.href="https://www.instagram.com/p/CYm7ygDvG83/?utm_source=ig_web_copy_link"
+    phone.appendChild(a)
+    })
+    phone.addEventListener('mouseleave', function(){
+    phone.style.backgroundColor="#ff38dd";
+    phone.innerHTML=""
+    })
+}
+
+microphoneSquare();
 
 const peaceSquare = () => {
     const peace = document.getElementById("peace")
@@ -59,3 +95,26 @@ peace.addEventListener('mouseleave', function(){
 }
 
 peaceSquare();
+
+const clickMe = () => {
+const clickSquares = document.getElementsByClassName("click-me")
+console.log(clickSquares)
+const squareArray = Array.from(clickSquares);
+
+squareArray.map(function(x) {
+    if (x.style.backgroundColor="black"){
+     const p = document.createElement('p')
+        p.innerText="😎"
+        p.style.color="white"
+        x.appendChild(p)
+        }
+        
+    })
+}
+
+setTimeout(clickMe, 3000)
+
+
+
+
+
